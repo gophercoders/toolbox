efd265d783d91458f8bbebdc97c343d36c46e412
Owen Waller <o.waller@kulawe.com>
2016-03-03 21:14:56 +0000

    Move the palette into the toolbox
    Moved the palette into the toolbox form the mandelbrot project.
    Also the palette has been updated to use the toolbox.Colour type.

---
+++ palette/palette.go
@@ -0,0 +1,424 @@
+package palette
+
+import "github.com/gophercoders/toolbox"
+
+var (
+	Black                = toolbox.Colour{0x00, 0x00, 0x00, 0xFF}
+	Navy                 = toolbox.Colour{0x00, 0x00, 0x80, 0xFF}
+	DarkBlue             = toolbox.Colour{0x00, 0x00, 0x8B, 0xFF}
+	MediumBlue           = toolbox.Colour{0x00, 0x00, 0xCD, 0xFF}
+	Blue                 = toolbox.Colour{0x00, 0x00, 0xFF, 0xFF}
+	DarkGreen            = toolbox.Colour{0x00, 0x64, 0x00, 0xFF}
+	Green                = toolbox.Colour{0x00, 0x80, 0x00, 0xFF}
+	Teal                 = toolbox.Colour{0x00, 0x80, 0x80, 0xFF}
+	DarkCyan             = toolbox.Colour{0x00, 0x8B, 0x8B, 0xFF}
+	DeepSkyBlue          = toolbox.Colour{0x00, 0xBF, 0xFF, 0xFF}
+	DarkTurquoise        = toolbox.Colour{0x00, 0xCE, 0xD1, 0xFF}
+	MediumSpringGreen    = toolbox.Colour{0x00, 0xFA, 0x9A, 0xFF}
+	Lime                 = toolbox.Colour{0x00, 0xFF, 0x00, 0xFF}
+	SpringGreen          = toolbox.Colour{0x00, 0xFF, 0x7F, 0xFF}
+	Cyan                 = toolbox.Colour{0x00, 0xFF, 0xFF, 0xFF}
+	MidnightBlue         = toolbox.Colour{0x19, 0x19, 0x70, 0xFF}
+	DodgerBlue           = toolbox.Colour{0x1E, 0x90, 0xFF, 0xFF}
+	LightSeaGreen        = toolbox.Colour{0x20, 0xB2, 0xAA, 0xFF}
+	ForestGreen          = toolbox.Colour{0x22, 0x8B, 0x22, 0xFF}
+	SeaGreen             = toolbox.Colour{0x2E, 0x8B, 0x57, 0xFF}
+	DarkSlateGray        = toolbox.Colour{0x2F, 0x4F, 0x4F, 0xFF}
+	LimeGreen            = toolbox.Colour{0x32, 0xCD, 0x32, 0xFF}
+	MediumSeaGreen       = toolbox.Colour{0x3C, 0xB3, 0x71, 0xFF}
+	Turquoise            = toolbox.Colour{0x40, 0xE0, 0xD0, 0xFF}
+	RoyalBlue            = toolbox.Colour{0x41, 0x69, 0xE1, 0xFF}
+	SteelBlue            = toolbox.Colour{0x46, 0x82, 0xB4, 0xFF}
+	DarkSlateBlue        = toolbox.Colour{0x48, 0x3D, 0x8B, 0xFF}
+	MediumTurquoise      = toolbox.Colour{0x48, 0xD1, 0xCC, 0xFF}
+	Indigo               = toolbox.Colour{0x4B, 0x00, 0x82, 0xFF}
+	DarkOliveGreen       = toolbox.Colour{0x55, 0x6B, 0x2F, 0xFF}
+	CadetBlue            = toolbox.Colour{0x5F, 0x9E, 0xA0, 0xFF}
+	CornflowerBlue       = toolbox.Colour{0x64, 0x95, 0xED, 0xFF}
+	MediumAquamarine     = toolbox.Colour{0x66, 0xCD, 0xAA, 0xFF}
+	DimGray              = toolbox.Colour{0x69, 0x69, 0x69, 0xFF}
+	SlateBlue            = toolbox.Colour{0x6A, 0x5A, 0xCD, 0xFF}
+	OliveDrab            = toolbox.Colour{0x6B, 0x8E, 0x23, 0xFF}
+	SlateGray            = toolbox.Colour{0x70, 0x80, 0x90, 0xFF}
+	LightSlateGray       = toolbox.Colour{0x77, 0x88, 0x99, 0xFF}
+	MediumSlateBlue      = toolbox.Colour{0x7B, 0x68, 0xEE, 0xFF}
+	LawnGreen            = toolbox.Colour{0x7C, 0xFC, 0x00, 0xFF}
+	Chartreuse           = toolbox.Colour{0x7F, 0xFF, 0x00, 0xFF}
+	Aquamarine           = toolbox.Colour{0x7F, 0xFF, 0xD4, 0xFF}
+	Maroon               = toolbox.Colour{0x80, 0x00, 0x00, 0xFF}
+	Purple               = toolbox.Colour{0x80, 0x00, 0x80, 0xFF}
+	Olive                = toolbox.Colour{0x80, 0x80, 0x00, 0xFF}
+	Gray                 = toolbox.Colour{0x80, 0x80, 0x80, 0xFF}
+	SkyBlue              = toolbox.Colour{0x87, 0xCE, 0xEB, 0xFF}
+	LightSkyBlue         = toolbox.Colour{0x87, 0xCE, 0xFA, 0xFF}
+	BlueViolet           = toolbox.Colour{0x8A, 0x2B, 0xE2, 0xFF}
+	DarkRed              = toolbox.Colour{0x8B, 0x00, 0x00, 0xFF}
+	DarkMagenta          = toolbox.Colour{0x8B, 0x00, 0x8B, 0xFF}
+	SaddleBrown          = toolbox.Colour{0x8B, 0x45, 0x13, 0xFF}
+	DarkSeaGreen         = toolbox.Colour{0x8F, 0xBC, 0x8F, 0xFF}
+	LightGreen           = toolbox.Colour{0x90, 0xEE, 0x90, 0xFF}
+	MediumPurple         = toolbox.Colour{0x93, 0x70, 0xDB, 0xFF}
+	DarkViolet           = toolbox.Colour{0x94, 0x00, 0xD3, 0xFF}
+	PaleGreen            = toolbox.Colour{0x98, 0xFB, 0x98, 0xFF}
+	DarkOrchid           = toolbox.Colour{0x99, 0x32, 0xCC, 0xFF}
+	YellowGreen          = toolbox.Colour{0x9A, 0xCD, 0x32, 0xFF}
+	Sienna               = toolbox.Colour{0xA0, 0x52, 0x2D, 0xFF}
+	Brown                = toolbox.Colour{0xA5, 0x2A, 0x2A, 0xFF}
+	DarkGray             = toolbox.Colour{0xA9, 0xA9, 0xA9, 0xFF}
+	LightBlue            = toolbox.Colour{0xAD, 0xD8, 0xE6, 0xFF}
+	GreenYellow          = toolbox.Colour{0xAD, 0xFF, 0x2F, 0xFF}
+	PaleTurquoise        = toolbox.Colour{0xAF, 0xEE, 0xEE, 0xFF}
+	LightSteelBlue       = toolbox.Colour{0xB0, 0xC4, 0xDE, 0xFF}
+	PowderBlue           = toolbox.Colour{0xB0, 0xE0, 0xE6, 0xFF}
+	FireBrick            = toolbox.Colour{0xB2, 0x22, 0x22, 0xFF}
+	DarkGoldenrod        = toolbox.Colour{0xB8, 0x86, 0x0B, 0xFF}
+	MediumOrchid         = toolbox.Colour{0xBA, 0x55, 0xD3, 0xFF}
+	RosyBrown            = toolbox.Colour{0xBC, 0x8F, 0x8F, 0xFF}
+	DarkKhaki            = toolbox.Colour{0xBD, 0xB7, 0x6B, 0xFF}
+	Silver               = toolbox.Colour{0xC0, 0xC0, 0xC0, 0xFF}
+	MediumVioletRed      = toolbox.Colour{0xC7, 0x15, 0x85, 0xFF}
+	IndianRed            = toolbox.Colour{0xCD, 0x5C, 0x5C, 0xFF}
+	Peru                 = toolbox.Colour{0xCD, 0x85, 0x3F, 0xFF}
+	Chocolate            = toolbox.Colour{0xD2, 0x69, 0x1E, 0xFF}
+	Tan                  = toolbox.Colour{0xD2, 0xB4, 0x8C, 0xFF}
+	LightGrey            = toolbox.Colour{0xD3, 0xD3, 0xD3, 0xFF}
+	Thistle              = toolbox.Colour{0xD8, 0xBF, 0xD8, 0xFF}
+	Orchid               = toolbox.Colour{0xDA, 0x70, 0xD6, 0xFF}
+	Goldenrod            = toolbox.Colour{0xDA, 0xA5, 0x20, 0xFF}
+	PaleVioletRed        = toolbox.Colour{0xDB, 0x70, 0x93, 0xFF}
+	Crimson              = toolbox.Colour{0xDC, 0x14, 0x3C, 0xFF}
+	Gainsboro            = toolbox.Colour{0xDC, 0xDC, 0xDC, 0xFF}
+	Plum                 = toolbox.Colour{0xDD, 0xA0, 0xDD, 0xFF}
+	BurlyWood            = toolbox.Colour{0xDE, 0xB8, 0x87, 0xFF}
+	LightCyan            = toolbox.Colour{0xE0, 0xFF, 0xFF, 0xFF}
+	Lavender             = toolbox.Colour{0xE6, 0xE6, 0xFA, 0xFF}
+	DarkSalmon           = toolbox.Colour{0xE9, 0x96, 0x7A, 0xFF}
+	Violet               = toolbox.Colour{0xEE, 0x82, 0xEE, 0xFF}
+	PaleGoldenrod        = toolbox.Colour{0xEE, 0xE8, 0xAA, 0xFF}
+	LightCoral           = toolbox.Colour{0xF0, 0x80, 0x80, 0xFF}
+	Khaki                = toolbox.Colour{0xF0, 0xE6, 0x8C, 0xFF}
+	AliceBlue            = toolbox.Colour{0xF0, 0xF8, 0xFF, 0xFF}
+	Honeydew             = toolbox.Colour{0xF0, 0xFF, 0xF0, 0xFF}
+	Azure                = toolbox.Colour{0xF0, 0xFF, 0xFF, 0xFF}
+	SandyBrown           = toolbox.Colour{0xF4, 0xA4, 0x60, 0xFF}
+	Wheat                = toolbox.Colour{0xF5, 0xDE, 0xB3, 0xFF}
+	Beige                = toolbox.Colour{0xF5, 0xF5, 0xDC, 0xFF}
+	WhiteSmoke           = toolbox.Colour{0xF5, 0xF5, 0xF5, 0xFF}
+	MintCream            = toolbox.Colour{0xF5, 0xFF, 0xFA, 0xFF}
+	GhostWhite           = toolbox.Colour{0xF8, 0xF8, 0xFF, 0xFF}
+	Salmon               = toolbox.Colour{0xFA, 0x80, 0x72, 0xFF}
+	AntiqueWhite         = toolbox.Colour{0xFA, 0xEB, 0xD7, 0xFF}
+	Linen                = toolbox.Colour{0xFA, 0xF0, 0xE6, 0xFF}
+	LightGoldenrodYellow = toolbox.Colour{0xFA, 0xFA, 0xD2, 0xFF}
+	OldLace              = toolbox.Colour{0xFD, 0xF5, 0xE6, 0xFF}
+	Red                  = toolbox.Colour{0xFF, 0x00, 0x00, 0xFF}
+	Magenta              = toolbox.Colour{0xFF, 0x00, 0xFF, 0xFF}
+	DeepPink             = toolbox.Colour{0xFF, 0x14, 0x93, 0xFF}
+	OrangeRed            = toolbox.Colour{0xFF, 0x45, 0x00, 0xFF}
+	Tomato               = toolbox.Colour{0xFF, 0x63, 0x47, 0xFF}
+	HotPink              = toolbox.Colour{0xFF, 0x69, 0xB4, 0xFF}
+	Coral                = toolbox.Colour{0xFF, 0x7F, 0x50, 0xFF}
+	DarkOrange           = toolbox.Colour{0xFF, 0x8C, 0x00, 0xFF}
+	LightSalmon          = toolbox.Colour{0xFF, 0xA0, 0x7A, 0xFF}
+	Orange               = toolbox.Colour{0xFF, 0xA5, 0x00, 0xFF}
+	LightPink            = toolbox.Colour{0xFF, 0xB6, 0xC1, 0xFF}
+	Pink                 = toolbox.Colour{0xFF, 0xC0, 0xCB, 0xFF}
+	Gold                 = toolbox.Colour{0xFF, 0xD7, 0x00, 0xFF}
+	PeachPuff            = toolbox.Colour{0xFF, 0xDA, 0xB9, 0xFF}
+	NavajoWhite          = toolbox.Colour{0xFF, 0xDE, 0xAD, 0xFF}
+	Moccasin             = toolbox.Colour{0xFF, 0xE4, 0xB5, 0xFF}
+	Bisque               = toolbox.Colour{0xFF, 0xE4, 0xC4, 0xFF}
+	MistyRose            = toolbox.Colour{0xFF, 0xE4, 0xE1, 0xFF}
+	BlanchedAlmond       = toolbox.Colour{0xFF, 0xEB, 0xCD, 0xFF}
+	PapayaWhip           = toolbox.Colour{0xFF, 0xEF, 0xD5, 0xFF}
+	LavenderBlush        = toolbox.Colour{0xFF, 0xF0, 0xF5, 0xFF}
+	Seashell             = toolbox.Colour{0xFF, 0xF5, 0xEE, 0xFF}
+	Cornsilk             = toolbox.Colour{0xFF, 0xF8, 0xDC, 0xFF}
+	LemonChiffon         = toolbox.Colour{0xFF, 0xFA, 0xCD, 0xFF}
+	FloralWhite          = toolbox.Colour{0xFF, 0xFA, 0xF0, 0xFF}
+	Snow                 = toolbox.Colour{0xFF, 0xFA, 0xFA, 0xFF}
+	Yellow               = toolbox.Colour{0xFF, 0xFF, 0x00, 0xFF}
+	LightYellow          = toolbox.Colour{0xFF, 0xFF, 0xE0, 0xFF}
+	Ivory                = toolbox.Colour{0xFF, 0xFF, 0xF0, 0xFF}
+	White                = toolbox.Colour{0xFF, 0xFF, 0xFF, 0xFF}
+)
+
+// this is a blue palette
+var BluePalette = [...]toolbox.Colour{Black,
+	Navy,
+	DarkBlue,
+	MediumBlue,
+	Blue,
+	DarkGreen,
+	Green,
+	Teal,
+	DarkCyan,
+	DeepSkyBlue,
+	DarkTurquoise,
+	MediumSpringGreen,
+	Lime,
+	SpringGreen,
+	Cyan,
+	MidnightBlue,
+	DodgerBlue,
+	LightSeaGreen,
+	ForestGreen,
+	SeaGreen,
+	DarkSlateGray,
+	LimeGreen,
+	MediumSeaGreen,
+	Turquoise,
+	RoyalBlue,
+	SteelBlue,
+	DarkSlateBlue,
+	MediumTurquoise,
+	Indigo,
+	DarkOliveGreen,
+	CadetBlue,
+	CornflowerBlue,
+	MediumAquamarine,
+	DimGray,
+	SlateBlue,
+	OliveDrab,
+	SlateGray,
+	LightSlateGray,
+	MediumSlateBlue,
+	LawnGreen,
+	Chartreuse,
+	Aquamarine,
+	Maroon,
+	Purple,
+	Olive,
+	Gray,
+	SkyBlue,
+	LightSkyBlue,
+	BlueViolet,
+	DarkRed,
+	DarkMagenta,
+	SaddleBrown,
+	DarkSeaGreen,
+	LightGreen,
+	MediumPurple,
+	DarkViolet,
+	PaleGreen,
+	DarkOrchid,
+	YellowGreen,
+	Sienna,
+	Brown,
+	DarkGray,
+	LightBlue,
+	GreenYellow,
+	PaleTurquoise,
+	LightSteelBlue,
+	PowderBlue,
+	FireBrick,
+	DarkGoldenrod,
+	MediumOrchid,
+	RosyBrown,
+	DarkKhaki,
+	Silver,
+	MediumVioletRed,
+	IndianRed,
+	Peru,
+	Chocolate,
+	Tan,
+	LightGrey,
+	Thistle,
+	Orchid,
+	Goldenrod,
+	PaleVioletRed,
+	Crimson,
+	Gainsboro,
+	Plum,
+	BurlyWood,
+	LightCyan,
+	Lavender,
+	DarkSalmon,
+	Violet,
+	PaleGoldenrod,
+	LightCoral,
+	Khaki,
+	AliceBlue,
+	Honeydew,
+	Azure,
+	SandyBrown,
+	Wheat,
+	Beige,
+	WhiteSmoke,
+	MintCream,
+	GhostWhite,
+	Salmon,
+	AntiqueWhite,
+	Linen,
+	LightGoldenrodYellow,
+	OldLace,
+	Red,
+	Magenta,
+	DeepPink,
+	OrangeRed,
+	Tomato,
+	HotPink,
+	Coral,
+	DarkOrange,
+	LightSalmon,
+	Orange,
+	LightPink,
+	Pink,
+	Gold,
+	PeachPuff,
+	NavajoWhite,
+	Moccasin,
+	Bisque,
+	MistyRose,
+	BlanchedAlmond,
+	PapayaWhip,
+	LavenderBlush,
+	Seashell,
+	Cornsilk,
+	LemonChiffon,
+	FloralWhite,
+	Snow,
+	Yellow,
+	LightYellow,
+	Ivory,
+	White}
+
+// This is the red palette
+var RedPalette = [...]toolbox.Colour{Black,
+	IndianRed,
+	LightCoral,
+	Salmon,
+	DarkSalmon,
+	Red,
+	Crimson,
+	FireBrick,
+	DarkRed,
+	Pink,
+	LightPink,
+	HotPink,
+	DeepPink,
+	MediumVioletRed,
+	PaleVioletRed,
+	LightSalmon,
+	Coral,
+	Tomato,
+	OrangeRed,
+	DarkOrange,
+	Orange,
+	Gold,
+	Yellow,
+	LightYellow,
+	LemonChiffon,
+	LightGoldenrodYellow,
+	PapayaWhip,
+	Moccasin,
+	PeachPuff,
+	PaleGoldenrod,
+	Khaki,
+	DarkKhaki,
+	Cornsilk,
+	BlanchedAlmond,
+	Bisque,
+	NavajoWhite,
+	Wheat,
+	BurlyWood,
+	Tan,
+	RosyBrown,
+	SandyBrown,
+	Goldenrod,
+	DarkGoldenrod,
+	Peru,
+	Chocolate,
+	SaddleBrown,
+	Sienna,
+	Brown,
+	Maroon,
+	Lavender,
+	Thistle,
+	Plum,
+	Violet,
+	Orchid,
+	Magenta,
+	MediumOrchid,
+	MediumPurple,
+	BlueViolet,
+	DarkViolet,
+	DarkOrchid,
+	DarkMagenta,
+	Purple,
+	Indigo,
+	DarkSlateBlue,
+	SlateBlue,
+	MediumSlateBlue,
+	GreenYellow,
+	Chartreuse,
+	LawnGreen,
+	Lime,
+	LimeGreen,
+	PaleGreen,
+	LightGreen,
+	MediumSpringGreen,
+	SpringGreen,
+	MediumSeaGreen,
+	SeaGreen,
+	ForestGreen,
+	Green,
+	DarkGreen,
+	YellowGreen,
+	OliveDrab,
+	Olive,
+	DarkOliveGreen,
+	MediumAquamarine,
+	DarkSeaGreen,
+	LightSeaGreen,
+	DarkCyan,
+	Teal,
+	Cyan,
+	LightCyan,
+	PaleTurquoise,
+	Aquamarine,
+	Turquoise,
+	MediumTurquoise,
+	DarkTurquoise,
+	CadetBlue,
+	SteelBlue,
+	LightSteelBlue,
+	PowderBlue,
+	LightBlue,
+	SkyBlue,
+	LightSkyBlue,
+	DeepSkyBlue,
+	DodgerBlue,
+	CornflowerBlue,
+	RoyalBlue,
+	Blue,
+	MediumBlue,
+	DarkBlue,
+	Navy,
+	MidnightBlue,
+	Snow,
+	Honeydew,
+	MintCream,
+	Azure,
+	AliceBlue,
+	GhostWhite,
+	WhiteSmoke,
+	Seashell,
+	Beige,
+	OldLace,
+	FloralWhite,
+	Ivory,
+	AntiqueWhite,
+	Linen,
+	LavenderBlush,
+	MistyRose,
+	Gainsboro,
+	LightGrey,
+	Silver,
+	DarkGray,
+	Gray,
+	DimGray,
+	LightSlateGray,
+	SlateGray,
+	DarkSlateGray,
+	White}
